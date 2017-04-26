2e59f968a210c7887e681e115a61e2e5855e2a25
Jiar <jiar.world@gmail.com>
2017-04-27 00:36:06 +0800

    修复节点列表显示bug

--- Admin/src/store/mutation-types.js
+++ Admin/src/store/mutation-types.js
@@ -3,10 +3,10 @@ export const USER_SIGNUP = 'USER_SIGNUP'
 export const USER_SIGNIN = 'USER_SIGNIN'
 export const USER_USER = 'USER_USER'
 export const USER_USERS = 'USER_USERS'
-// sort
-export const SORT_CREATEDSORT = 'SORT_CREATEDSORT'
-export const SORT_SORT = 'SORT_SORT'
-export const SORT_SORTS = 'SORT_SORTS'
+// node
+export const NODE_CREATEDNODE = 'NODE_CREATEDNODE'
+export const NODE_NODE = 'NODE_NODE'
+export const NODE_NODES = 'NODE_NODES'
 // topic
 export const TOPIC_CREATEDTOPIC = 'TOPIC_CREATEDTOPIC'
 export const TOPIC_TOPIC = 'TOPIC_TOPIC'
